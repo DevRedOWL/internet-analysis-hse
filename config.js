95ccc04867bc0859aebac12657241ada5d580efe
Daniel <devredowl@gmail.com>
2022-11-24 02:18:22 +0300

    Once again

--- config.js
+++ config.js
@@ -11,8 +11,8 @@ export const db = {
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST | 'localhost',
-  port: process.env.DB_PORT | '5432',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || '5432',
 };
 
 export const credentials = {
